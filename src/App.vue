c1f40ff2e2b374eaf8aa50cd428d978f6ad7de7d
mirkone1994 <mirkochiappori@gmail.com>
2021-09-10 12:39:52 +0200

    imported axios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Header/>
+    <Films/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue"
+import Films from "./components/Films.vue"
 export default {
   name: 'App',
   components: {
     Header,
+    Films,
     
   }
 }
---
+++ src/components/Films.vue
@@ -0,0 +1,30 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name:"Films",
+    data(){
+        return {
+            baseUri : "https://api.themoviedb.org/3",
+            apiKey : "a21c1888019d78a2591d2a3dc243bfe1",
+            query : "pinocchio",
+            films : [],
+        }
+    },
+    created(){
+        axios.get(`${this.baseUri}/search/movie?api_key=${this.apiKey}&query=${this.query}`).then((res)=>
+        {
+            this.films=res.data.results;
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
