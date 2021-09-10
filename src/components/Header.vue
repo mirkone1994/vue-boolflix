ac2a0c9fdec718df9e004d79b51a920148afee5a
mirkone1994 <mirkochiappori@gmail.com>
2021-09-10 12:01:38 +0200

    created search bar and its component

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container">
+      <div class="row">
+          <div class="col-3">
+              <span>BOOLFLIX</span>
+          </div>
+          <div class="col-3">
+              <Search/>
+
+          </div>
+      </div>
+
+  </section>
+</template>
+
+<script>
+import Search from "../components/Search.vue"
+export default {
+    name:"Header",
+    components: {
+        Search,
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+    max-width: 1400px;
+    margin: auto;
+    background-color: #000;
+}
+.row {
+    display: flex;
+    justify-content: space-around;
+}
+span {
+    color: red;
+}
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+      <input type="text" placeholder="Cerca il film...">
+      <button type="button">Cerca</button>
+  </section>
+</template>
+
+<script>
+export default {
+name:"Search"
+}
+</script>
+
+<style>
+
+</style>
