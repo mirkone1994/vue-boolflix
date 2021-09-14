0eb5618ee9e39fcb92be083e021640319798c101
mirkone1994 <mirkochiappori@gmail.com>
2021-09-14 15:52:47 +0200

    added rotation cards

--- src/components/FilmsCard.vue
+++ src/components/FilmsCard.vue
@@ -1,14 +1,24 @@
 <template>
   <section class="films-card">
-      <img :src="poster(film.poster_path)" alt="">
-      <span>{{film.title}}</span>
-      <span>{{film.original_title}}</span>
-      <img class="flag" v-if="flags.includes(film.original_language)" :src="flag(film.original_language)" :alt="film.title">
-      <span v-else>{{film.original_language}}</span>
-      <span class="star" v-for="star in 5" :key="star">
-        <i v-if="star<=isWhole(film.vote_average)" class="fas fa-star"></i>
-        <i v-else class="far fa-star"></i>
-      </span>
+    <div class="container">
+      <div class="deck">
+        <div class="card hovercard">
+          <div class="front face">
+            <img :src="poster(film.poster_path)" alt="">
+          </div>
+          <div class="back face">
+            <span>{{film.title}}</span>
+            <span>{{film.original_title}}</span>
+            <img class="flag" v-if="flags.includes(film.original_language)" :src="flag(film.original_language)" :alt="film.title">
+            <span v-else>{{film.original_language}}</span>
+            <span class="star" v-for="star in 5" :key="star">
+              <i v-if="star<=isWhole(film.vote_average)" class="fas fa-star"></i>
+              <i v-else class="far fa-star"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -47,5 +57,45 @@ span {
 }
 .star {
   display: inline-block;
+}
+.deck{
+  margin:15px;
+  width:341px;
+  height:515px;
+  position:relative;
+  border-radius:10px;
+}
+.card{
+  width:100%;
+  height:100%;
+  -webkit-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+  -webkit-transition: all .5s linear;
+  transition: all .5s linear;
+}
+.face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  background-color:#fff;  
+}
+.back {
+  overflow:hidden;
+  z-index:-1;
+  display: block;
+  -webkit-transform: rotateY(180deg);
+  transform: rotateY(180deg);
+}
+.front, .back{
+  transition: all .5s linear;
+}
+.front{
+  z-index:1;
+}
+.hovercard:hover{
+  -webkit-transform: rotateY(180deg);
+  transform: rotateY(180deg);
 }
 </style>
--- src/components/SeriesCard.vue
+++ src/components/SeriesCard.vue
@@ -1,16 +1,27 @@
 <template>
-  <section class="series-card">
-      <img class="poster" :src="poster(serie.poster_path)" alt="">
-      <span>{{serie.name}}</span>
-      <span>{{serie.original_name}}</span>
-      <img class="flag" v-if="flags.includes(serie.original_language)" :src="flag(serie.original_language)" :alt="serie.title">
-      <span v-else>{{serie.original_language}}</span>
-      <span class="star" v-for="star in 5" :key="star">
-        <i v-if="star<=isWhole(serie.vote_average)" class="fas fa-star"></i>
-        <i v-else class="far fa-star"></i>
-      </span>
+  <section class="films-card">
+    <div class="container">
+      <div class="deck">
+        <div class="card hovercard">
+          <div class="front face">
+            <img class="poster" :src="poster(serie.poster_path)" alt="">
+          </div>
+          <div class="back face">
+            <span>{{serie.name}}</span>
+            <span>{{serie.original_name}}</span>
+            <img class="flag" v-if="flags.includes(serie.original_language)" :src="flag(serie.original_language)" :alt="serie.title">
+            <span v-else>{{serie.original_language}}</span>
+            <span class="star" v-for="star in 5" :key="star">
+              <i v-if="star<=isWhole(serie.vote_average)" class="fas fa-star"></i>
+              <i v-else class="far fa-star"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
+
 
 <script>
 export default {
@@ -47,5 +58,45 @@ span {
 }
 .star {
   display: inline-block;
+}
+.deck{
+  margin:15px;
+  width:341px;
+  height:515px;
+  position:relative;
+  border-radius:10px;
+}
+.card{
+  width:100%;
+  height:100%;
+  -webkit-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+  -webkit-transition: all .5s linear;
+  transition: all .5s linear;
+}
+.face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  background-color:#fff;  
+}
+.back {
+  overflow:hidden;
+  z-index:-1;
+  display: block;
+  -webkit-transform: rotateY(180deg);
+  transform: rotateY(180deg);
+}
+.front, .back{
+  transition: all .5s linear;
+}
+.front{
+  z-index:1;
+}
+.hovercard:hover{
+  -webkit-transform: rotateY(180deg);
+  transform: rotateY(180deg);
 }
 </style>
